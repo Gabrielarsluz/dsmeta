735b435ac9b43cb128ab990cd273e784bdcdd1d2
Gabrielarsluz <gabrielarsluz@gmail.com>
2022-09-10 21:27:29 -0300

    where is the list

--- frontend/src/Components/SalesCard/index.tsx
+++ frontend/src/Components/SalesCard/index.tsx
@@ -14,12 +14,15 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    console.log(
-      axios.get(`${BASE_URL}/sales`).then(Response => {
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then(Response => {
         setSales(Response.data.content);
-      })
-    );
-  }, []);
+      });
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
